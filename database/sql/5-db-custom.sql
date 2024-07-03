678abf8495b01fe889a41184d811ce230dc5d176
root <root@DESKTOP-9OOB4SB>
2024-07-03 17:45:25 +0900

    ssf friendly balance. increase global drop rates

---
+++ database/sql/5-db-custom.sql
@@ -0,0 +1,9 @@
+USE `cosmic`;
+
+REPLACE INTO `drop_data_global` (`id`, `continent`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`, `comments`) VALUES
+	(1, -1, 4031865, 1, 2, 0, 480000, 'NX Card 100 PTS'),
+	(2, -1, 4031866, 1, 2, 0, 160000, 'NX Card 250 PTS'),
+	(3, -1, 4001126, 1, 2, 0, 8000, 'Maple Leaves'),
+	(4, -1, 2049100, 1, 2, 0, 10000, 'Chaos Scroll 60%'),
+	(5, -1, 2340000, 1, 2, 0, 10000, 'White Scroll'),
+	(6, -1, 4001006, 1, 2, 0, 10000, 'Flaming Feather');
